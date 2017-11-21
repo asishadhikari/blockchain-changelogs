f0ae58f79af8eb9a0f07ac7d9eeab7d35783e439
sikari <ashish.adhikari@bison.howard.edu>
2017-11-20 22:32:32 -0500

    trial

---
+++ request_generator.js
@@ -0,0 +1,33 @@
+var http = require('http'),
+    querystring = require('querystring'); 
+require('request-to-curl');
+var data = require('./drivedata.js');
+console.log(data);
+ 
+var postData = JSON.stringify(data);
+
+var myport = process.env.HTTP_PORT || 3001;
+ 
+var options = {
+    hostname: 'localhost',
+    port: myport,
+    path: '/mineBlock',
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'Content-Length': postData.length
+    }
+};
+ 
+var req = http.request(options, (res) => {
+    console.log(req.toCurl());
+});
+ 
+req.on('error', (e) => {
+    console.log(`problem with request: ${e.message}`);
+});
+ 
+// write data to request body 
+req.write(postData);
+req.end();
+
